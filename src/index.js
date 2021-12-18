c5150b24356625817f4e72dd51d4833ac8d981b0
Pavlo-Zubovych <zu10@ukr.net>
2021-12-18 22:11:26 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-import './sass/main.scss';
-// Розібрати синтаксис scss,
-// Глянув
-// Передивитись відео
-// Розпочати домашку
+import './styles.css';
+import template from './gallery-items.hbs';
+import menu from './menu.json';
+
+console.log('jdfkskf??????????');
+
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+};
+
+const markUp = template(menu);
+refs.menuList.insertAdjacentHTML('beforeend', markUp);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const changeTheme = document.querySelector('#theme-switch-toggle');
+
+changeTheme.addEventListener('change', savedChangeTheme);
+const themeShow = document.querySelector('body');
+
+function defaultTheme() {
+  if (localStorage.getItem('Theme') === Theme.DARK) {
+    themeShow.classList.add(Theme.DARK);
+    changeTheme.checked = true;
+    themeShow.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.setItem('Theme', 'light-theme');
+    themeShow.classList.add(Theme.LIGHT);
+  }
+}
+defaultTheme();
+
+function savedChangeTheme(e) {
+  console.log(e);
+  if (e.target.checked) {
+    themeShow.classList.add(Theme.DARK);
+    themeShow.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', 'dark-theme');
+  } else if (!e.target.checked) {
+    themeShow.classList.add(Theme.LIGHT);
+    themeShow.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
